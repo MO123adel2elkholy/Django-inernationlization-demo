712b88fb6a2e48b45ed41546d8def4cf6d62e369
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2026-01-24 02:10:45 +0200

    adding message framework to flush success messages or error messages

--- language/views.py
+++ language/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Post
-# Create your views here.
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 
 
 def home(request):
+
+    if request.user.is_authenticated:
+        messages.success(request, _("authmessage {first} {last}").format(
+            first=request.user.first_name, last=request.user.last_name), extra_tags="alert alert-success")
+    else:
+        messages.warning(request, _("anonmessage"),
+                         extra_tags="alert alert-danger")
+
     posts = Post.objects.all()
-    return render(request, 'index.html', {'posts': posts})
+
+    context = {'posts': posts}
+    return render(request, 'index.html', context)
